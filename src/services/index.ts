cc9c0b90f9dfb545ee72bccf59075eb9f5729ac2
vitor <vitorsbsilva@gmail.com>
2022-07-08 16:32:55 -0300

    fix: exclude ServiceError from readOne return in generic Services

--- src/services/index.ts
+++ src/services/index.ts
@@ -16,7 +16,7 @@ abstract class Service<T> {
     return this.model.read();
   }
 
-  public async readOne(id: string): Promise<T | null | ServiceError> {
+  public async readOne(id: string): Promise<T | null> {
     return this.model.readOne(id);
   }
 
