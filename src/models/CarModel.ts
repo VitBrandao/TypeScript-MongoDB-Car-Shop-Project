2bfd2a72e0b7e01d5014c28aba2210856876fcfb
vitor <vitorsbsilva@gmail.com>
2022-07-08 12:18:56 -0300

    feat: Car Model

---
+++ src/models/CarModel.ts
@@ -0,0 +1,21 @@
+import { Schema, model as createModel } from 'mongoose';
+import MongoModel from '.';
+import { Car } from '../interfaces/CarInterface';
+
+const CarSchema = new Schema<Car>({
+  model: { type: String, required: true },
+  year: { type: Number, required: true },
+  color: { type: String, required: true },
+  status: { type: Boolean, required: false },
+  buyValue: { type: Number, required: true },
+  doorsQty: { type: Number, required: true },
+  seatsQty: { type: Number, required: true },
+});
+
+class CarModel extends MongoModel<Car> {
+  constructor(private carModel = createModel<Car>('Car', CarSchema)) {
+    super(carModel);
+  }
+}
+
+export default CarModel;
