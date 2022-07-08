76aa311779fdf39eb004a70de82bdf70d4b63439
vitor <vitorsbsilva@gmail.com>
2022-07-08 16:33:31 -0300

    add: new error to ControllerErros about id hexadecimal

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -14,6 +14,7 @@ enum ControllerErrors {
   notFound = 'Object not found',
   requiredId = 'Id is required',
   badRequest = 'Bad request',
+  idLengthError = 'Id must have 24 hexadecimal characters',
 }
 
 abstract class Controller<T> {
@@ -40,10 +41,10 @@ abstract class Controller<T> {
     }
   };
 
-  // abstract readOne(
-  //   req: Request<{ id: string; }>,
-  //   res: Response<T | ResponseError>
-  // ): Promise<typeof res>;
+  abstract readOne(
+    req: Request<{ id: string; }>,
+    res: Response<T | ResponseError>
+  ): Promise<typeof res>;
 
   // abstract update(
   //   req: RequestWithBody<T>,
