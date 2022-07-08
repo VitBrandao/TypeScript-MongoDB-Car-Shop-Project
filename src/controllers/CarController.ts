5713c4f738c522922eaee66378448b236e6dbfb9
vitor <vitorsbsilva@gmail.com>
2022-07-08 16:33:46 -0300

    feat: Car Controller readOne method

--- src/controllers/CarController.ts
+++ src/controllers/CarController.ts
@@ -1,4 +1,4 @@
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import Controller, { RequestWithBody, ResponseError } from '.';
 import { Car } from '../interfaces/CarInterface';
 import CarService from '../services/CarService';
@@ -32,6 +32,22 @@ class CarController extends Controller<Car> {
       return res.status(201).json(createdCar);
     } catch (err) {
       return res.status(400).json({ error: this.errors.internal });
+    }
+  };
+
+  readOne = async (
+    req: Request<{ id: string; }>,
+    res: Response<Car | ResponseError>,
+  ): Promise<typeof res> => {
+    const { id } = req.params;
+    try {
+      const carById = await this.service.readOne(id);
+      if (carById === null) {
+        return res.status(400).json({ error: this.errors.idLengthError });
+      }
+      return res.json(carById);
+    } catch (err) {
+      return res.status(404).json({ error: this.errors.notFound });
     }
   };
 }
