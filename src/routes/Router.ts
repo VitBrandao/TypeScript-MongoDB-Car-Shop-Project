053db2a1ca45427f42ba6604a62d6e774bf305cf
vitor <vitorsbsilva@gmail.com>
2022-07-08 16:59:51 -0300

    add: put/update route to Router

--- src/routes/Router.ts
+++ src/routes/Router.ts
@@ -15,7 +15,7 @@ class CustomRouter<T> {
     this.router.get(route, controller.read);
     this.router.post(route, controller.create);
     this.router.get(`${route}/:id`, controller.readOne);
-    // this.router.put(`${route}/:id`, controller.update);
+    this.router.put(`${route}/:id`, controller.update);
     // this.router.delete(`${route}/:id`, controller.delete);
   }
 }
