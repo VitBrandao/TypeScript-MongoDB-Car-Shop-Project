a9ab59caf7a71eb15a16126dc0c12d8d2cf10871
vitor <vitorsbsilva@gmail.com>
2022-07-08 11:10:14 -0300

    refactor: CarInterface to export schema

--- src/interfaces/CarInterface.ts
+++ src/interfaces/CarInterface.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 import { Vehicle } from './VehicleInterface';
 
-const CarSchema = z.object({
+export const CarSchema = z.object({
   doorsQty: z.number({
     required_error: 'DoorsQty is required',
     invalid_type_error: 'DoorsQty must be a number',
@@ -14,8 +14,3 @@ const CarSchema = z.object({
 });
 
 export type Car = z.infer<typeof CarSchema> & Vehicle;
-
-// export interface Car extends Vehicle {
-//   doorsQty: number;
-//   seatsQty: number;
-// }
